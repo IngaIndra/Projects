edfc6f960585edd0ac52efc78c5a1e8b90cb6149
Indra.U <indra.ulziibadrakh@example.com>
2023-03-01 17:54:30 +0800

    mysql notes

--- SQL/test.sql
+++ SQL/test.sql
@@ -10,4 +10,24 @@ insert into employees (emp_no, birth_date, first_name, last_name, gender, hire_d
         primary key (id)
     );
 
-    
+    CREATE TABLE category (
+        id INT auto_increment,
+        name VARCHAR(255),
+        slug VARCHAR(255) UNIQUE,
+        description TEXT, 
+        productCount INT UNSIGNED DEFAULT 0,
+        createdAt datetime, 
+        PRIMARY KEY (id)
+    );
+
+    insert into category (name, slug, createdAt) value (
+        'Shoes', 'shoes', now()
+    );
+
+    alter table category DROP COLUMN description;
+
+      alter table category ADD COLUMN imageUrl varchar(500);
+
+      alter table category drop COLUMN imageUrl;
+
+      alter table category change column imageUrl image varchar(200);
